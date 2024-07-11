b76c6c23ca58035de1005313647710589a6378f5
William Wong <compulim@hotmail.com>
2024-07-10 20:25:19 -0700

    Use rollup to build

---
+++ packages/bot/rollup.config.mjs
@@ -0,0 +1,24 @@
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    resolve({
+      preferBuiltins: true
+    }),
+    commonjs(),
+    typescript({
+      allowSyntheticDefaultImports: true
+    }),
+    json()
+  ],
+  external: []
+};
