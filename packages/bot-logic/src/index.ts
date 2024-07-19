71a33b8d8243e787e753de9fa8312febc8e63802
William Wong <compulim@hotmail.com>
2024-07-18 22:35:31 -0700

    Revert package-lock.json

--- packages/bot-logic/src/index.ts
+++ packages/bot-logic/src/index.ts
@@ -1,4 +1,6 @@
 import EchoBot from './bot/EchoBot/index.js';
-import MockBot from './bot/MockBot/index.js';
+// import MockBot from './bot/MockBot/index.js';
+
+const MockBot = EchoBot;
 
 export { EchoBot, MockBot };
