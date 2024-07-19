3d1588c71af2d5a480e1276ee0d66b183f1f9db9
William Wong <compulim@hotmail.com>
2024-07-18 23:07:19 -0700

    Use MockBot

--- packages/bot-logic/src/index.ts
+++ packages/bot-logic/src/index.ts
@@ -1,6 +1,4 @@
 import EchoBot from './bot/EchoBot/index.js';
-// import MockBot from './bot/MockBot/index.js';
-
-const MockBot = EchoBot;
+import MockBot from './bot/MockBot/index.js';
 
 export { EchoBot, MockBot };
