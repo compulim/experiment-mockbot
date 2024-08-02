61f84acc9c8f76f56f15f1fc11ae6cbcc4761faf
William Wong <compulim@hotmail.com>
2024-08-02 12:57:51 -0700

    Add sendTypingIndicator

--- packages/pages/src/app/WebChat.tsx
+++ packages/pages/src/app/WebChat.tsx
@@ -2,17 +2,17 @@ import ReactWebChat from 'botframework-webchat';
 import { memo, useMemo } from 'react';
 import useWebChatAdapters from './data/useWebChatAdapters';
 
-const USE_BUNDLE = (() => {
-  try {
-    return new URLSearchParams(location.search).has('bundle');
-  } catch {
-    return false;
-  }
-})();
+// const USE_BUNDLE = (() => {
+//   try {
+//     return new URLSearchParams(location.search).has('bundle');
+//   } catch {
+//     return false;
+//   }
+// })();
 
 export default memo(function WebChat() {
   const [webChatAdapters] = useWebChatAdapters();
   const key = useMemo(() => Date.now(), [webChatAdapters]);
 
-  return webChatAdapters && <ReactWebChat {...webChatAdapters} key={key} />;
+  return webChatAdapters && <ReactWebChat {...webChatAdapters} key={key} sendTypingIndicator={true} />;
 });
