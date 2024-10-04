4c8ccce367bb01fe1d4cc1fa3e95139cba2b59ef
William Wong <compulim@hotmail.com>
2024-10-03 22:12:45 -0700

    Send activity twice

--- packages/bot-logic/src/bot/MockBot/commands2/printCard.ts
+++ packages/bot-logic/src/bot/MockBot/commands2/printCard.ts
@@ -2,20 +2,19 @@ import { MessageFactory, TurnContext } from 'botbuilder';
 import getCardJSON from '../commands/Cards/getCardJSON.js';
 
 export default async function printCard(context: TurnContext, cardName: string = '') {
-  context.sendActivities([
-    MessageFactory.text(`Printing "${cardName}".`),
-    {
-      entities: [
-        {
-          '@context': 'https://schema.org',
-          '@id': '',
-          '@type': 'Message',
-          keywords: ['AllowCopy'],
-          type: 'https://schema.org/Message'
-        }
-      ],
-      text: `\`\`\`json\n${JSON.stringify(getCardJSON(cardName), null, 2)}\n\`\`\``,
-      type: 'message'
-    }
-  ]);
+  await context.sendActivity(MessageFactory.text(`Printing "${cardName}" card JSON.`));
+
+  await context.sendActivity({
+    entities: [
+      {
+        '@context': 'https://schema.org',
+        '@id': '',
+        '@type': 'Message',
+        keywords: ['AllowCopy'],
+        type: 'https://schema.org/Message'
+      }
+    ],
+    text: `\`\`\`json\n${JSON.stringify(getCardJSON(cardName), null, 2)}\n\`\`\``,
+    type: 'message'
+  });
 }
